81a095992d240e2b559494fec90c73a42b5305c6
taufiq72 <taufiqq1207@gmail.com>
2026-07-22 10:40:22 +0700

    menambahkan async function startserver

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+    });
+}
