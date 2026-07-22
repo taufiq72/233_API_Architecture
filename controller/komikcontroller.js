df7aa7c1eb0264041168c519a580848f0c9a6649
taufiq72 <taufiqq1207@gmail.com>
2026-07-22 11:08:11 +0700

    menambahkan getkomikbyid

--- controller/komikcontroller.js
+++ controller/komikcontroller.js
@@ -9,3 +9,17 @@ async function getAllkomik(req, res) {
         res.status(500).json({ error: 'Internal server error' });
     }
 }
+
+async function getkomikById(req, res) {
+    const { id } = req.params;
+    try {
+        const komik = await db.komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).json({ error: 'komik not found' });
+        }
+        res.status(200).json(komik);
+    } catch (err) {
+        console.error('Error fetching komik by ID:', err.message);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
